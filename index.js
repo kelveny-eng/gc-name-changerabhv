16eef610ae30726dde65bb537c415c8897bede00
kelveny-eng <abhvvyy@gmail.com>
2025-11-24 21:54:46 +0530

    Refactor Puppeteer script for Instagram automation

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-
 const puppeteer = require("puppeteer");
 const names = require("./names.json");
-const SESSIONID = "17843691127146670";
+
 (async () => {
-    const browser = await puppeteer.launch({
-        headless: true
-    });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
 
-    const page = await browser.newPage();
-    await page.setCookie({
+  const page = await browser.newPage();
+
+  // 🔥 Add Instagram Cookies here
+  await page.setCookie({
     name: "sessionid",
-    value: SESSIONID,
-    domain: ".instagram.com"
-});
-    await page.goto("https://www.instagram.com/accounts/login/");
+    value: "17843691127146670", // <-- Tumhara sessionId
+    domain: ".instagram.com",
+    path: "/",
+    httpOnly: true,
+    secure: true
+  });
 
-    console.log("⏳ Login manually required…");
-    await page.waitForTimeout(25000); 
+  await page.goto("https://www.instagram.com/");
+  await page.waitForTimeout(4000);
 
-    await page.goto("https://www.instagram.com/direct/t/1176649277224222/");
+  // Go to GC
+  await page.goto("https://www.instagram.com/direct/t/1176649277224222/");
+  await page.waitForTimeout(6000);
 
-    for (const newName of names) {
-        console.log("Changing name to:", newName);
+  for (const newName of names) {
+    console.log("Changing name to:", newName);
 
-        await page.waitForSelector("input[placeholder='Group Name']");
-        await page.click("input[placeholder='Group Name']", { clickCount: 3 });
-        await page.type("input[placeholder='Group Name']", newName);
+    await page.waitForSelector("input[placeholder='Group Name']");
+    await page.click("input[placeholder='Group Name']", { clickCount: 3 });
+    await page.type("input[placeholder='Group Name']", newName);
 
-        await page.waitForSelector("button[type='submit']");
-        await page.click("button[type='submit']");
+    await page.waitForSelector("button[type='submit']");
+    await page.click("button[type='submit']");
 
-        await page.waitForTimeout(4000);
-    }
+    await page.waitForTimeout(4000);
+  }
 
-    console.log("✔ All names applied!");
-    await browser.close();
+  console.log("✔ All names applied!");
+  await browser.close();
 })();
