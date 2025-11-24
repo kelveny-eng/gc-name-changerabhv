5b18c821f81aace1efc407fc837206cc18edff40
kelveny-eng <abhvvyy@gmail.com>
2025-11-24 22:19:51 +0530

    Refactor Instagram automation script for clarity

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const names = require("./names.json");
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: "new",
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
   const page = await browser.newPage();
 
-  // 🔥 Add Instagram Cookies here
+  // Cookie Login
   await page.setCookie({
     name: "sessionid",
-    value: "17843691127146670", // <-- Tumhara sessionId
+    value: "17843691127146670", // YOUR SESSION
     domain: ".instagram.com",
     path: "/",
     httpOnly: true,
@@ -20,25 +20,27 @@ const names = require("./names.json");
   });
 
   await page.goto("https://www.instagram.com/");
-  await page.waitForTimeout(4000);
+  await page.waitFor(5000);
 
-  // Go to GC
+  // GC Page
   await page.goto("https://www.instagram.com/direct/t/1176649277224222/");
-  await page.waitForTimeout(6000);
+  await page.waitFor(7000);
 
   for (const newName of names) {
     console.log("Changing name to:", newName);
 
-    await page.waitForSelector("input[placeholder='Group Name']");
-    await page.click("input[placeholder='Group Name']", { clickCount: 3 });
-    await page.type("input[placeholder='Group Name']", newName);
+    const input = "input[placeholder='Group Name']";
+    await page.waitForSelector(input);
+    await page.click(input, { clickCount: 3 });
+    await page.type(input, newName);
 
-    await page.waitForSelector("button[type='submit']");
-    await page.click("button[type='submit']");
+    const saveBtn = "button[type='submit']";
+    await page.waitForSelector(saveBtn);
+    await page.click(saveBtn);
 
-    await page.waitForTimeout(4000);
+    await page.waitFor(4000); // old compatible delay
   }
 
-  console.log("✔ All names applied!");
+  console.log("✔ Name rotation completed!");
   await browser.close();
 })();
